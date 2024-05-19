c3d8148748dad5fa7f86bbbb3a80b8b2ca84b863
Maxime <maxime.langlet@hotmail.com>
2024-05-19 15:55:06 +0200

    bug defausse reglé

--- click.py
+++ click.py
@@ -244,6 +244,7 @@ def retourne_cartes(joueur, ecran,partie):
             affiche_aide(ecran, h, section=3) 
         else:
             cache_aide(ecran,l,h)
+
         pygame.event.get()
         s = pygame.mouse.get_pressed()
 
@@ -253,7 +254,7 @@ def retourne_cartes(joueur, ecran,partie):
                 test_appel_loupe(ecran,partie,joueur)
             for i in range(3):
                 for j in range(4):
-                    if(ecart + j * l+j*20< pos[0] <30 + j * l+j*20 +l) and (ecart + i * h+i*15< pos[1] <  30 + i * h+i*15 + h):
+                    if(ecart + j * l+j*20< pos[0] <ecart + j * l+j*20 +l) and (30 + i * h+i*15< pos[1] <  30 + i * h+i*15 + h):
                         if joueur.jeu_actuel[i][j].etat != "ouverte":
                             joueur.jeu_actuel[i][j].etat = "ouverte"
                             carte_selectionner = True
